b776eb7af97e466c9393a737dfd36e9c44b68aac
saroshkhann <ksarosh785@gmail.com>
2026-04-28 22:40:43 +0500

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,14 @@
-def bubble_sort(arr):
-    n = len(arr)
+unsorted_list = [12, 25, 11, 34, 90, 22]
+n = len(unsorted_list)
 
-    for passes in range(0,n):
-        for j in range(0,n-1-passes):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
 
+def binary_search(arr):
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
 
-unsorted_list = [120,25,11,34,90,22]
-sorted_list = bubble_sort(unsorted_list)
+sorted_list = binary_search(unsorted_list)
 print(sorted_list)
