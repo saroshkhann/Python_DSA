2d67032ea49171978c5e240fbf8ec546a80432d0
saroshkhann <ksarosh785@gmail.com>
2026-05-07 22:36:33 +0500

    Selection sort Algo

---
+++ selection_sort2.py
@@ -0,0 +1,17 @@
+unsorted_list = [12,4,5,1,4,77,86,33]
+
+def selection_sort(arr):
+    n =len(arr)
+
+    for i in range(n-1):
+        mid_index = i
+
+        for j in range(i+1, n):
+            if arr[mid_index] > arr[j]:
+                mid_index=j
+
+        arr[i], arr[mid_index] = arr[mid_index], arr[i]
+    return arr
+
+result = selection_sort(unsorted_list)
+print(result)
